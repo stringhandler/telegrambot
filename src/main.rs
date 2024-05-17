ca0e23c7c46c93c9bf03c779bfdebd027da10159
stringhandler <stringhandler@protonmail.com>
2024-05-17 15:48:11 +0200

    find burn working

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,11 @@
+use dotenv::dotenv;
+use minotari_node_grpc_client::grpc::FetchMatchingUtxosRequest;
 use minotari_node_grpc_client::grpc::SearchUtxosRequest;
 use minotari_node_grpc_client::BaseNodeGrpcClient;
+use tari_crypto::commitment;
+use tari_crypto::ristretto::{RistrettoComSig, RistrettoPublicKey, RistrettoSecretKey};
 use tari_utilities::hex::Hex;
+use tari_utilities::ByteArray;
 use teloxide::types::{Message, User};
 use teloxide::{prelude::*, RequestError};
 use tokio_stream::StreamExt;
@@ -76,21 +81,80 @@ use tokio_stream::StreamExt;
 //     .await;
 // }
 use anyhow::anyhow;
+use tari_crypto::commitment::HomomorphicCommitment;
 
-async fn check_commitment_exists(commitment: &str) -> Result<bool, anyhow::Error> {
+async fn check_commitment_exists(commitment_and_signature: &str) -> Result<bool, anyhow::Error> {
     let mut client = BaseNodeGrpcClient::connect("http://127.0.0.1:18182").await?;
+    let regex = regex::Regex::new(r"^[0-9a-fA-F]{64}$").unwrap();
+
+    let commitment = regex
+        .find(commitment_and_signature)
+        .ok_or_else(|| anyhow!("No commitment found"))?
+        .as_str();
+    dbg!(commitment);
+
+    // let split = commitment_and_signature.split("::");
+    // let collect: Vec<&str> = split.collect();
+    // if collect.len() != 4 {
+    //     return Err(anyhow!("Invalid commitment and signature"));
+    // }
+    // let commitment = collect[0];
+    // let signature_nonce =
+    //     Vec::<u8>::from_hex(collect[1]).map_err(|_| anyhow!("bad hex signature_nonce"))?;
+    // let signature_u =
+    //     Vec::<u8>::from_hex(collect[2]).map_err(|_| anyhow!("bad hex signature_u"))?;
+    // let signature_v =
+    //     Vec::<u8>::from_hex(collect[3]).map_err(|_| anyhow!("bad hex signature_v"))?;
+
+    let commitment_bytes =
+        Vec::<u8>::from_hex(commitment).map_err(|e| anyhow!("bad hex commitment"))?;
+
+    dbg!(&commitment_bytes);
+    // let pub_key_bytes = RistrettoPublicKey::from_canonical_bytes(&commitment_bytes)
+    //     .map_err(|_| anyhow!("commitment is not a pub key"))?;
+    // let comm_sig = RistrettoComSig::new(
+    //     HomomorphicCommitment::from_public_key(
+    //         &RistrettoPublicKey::from_canonical_bytes(&signature_nonce)
+    //             .map_err(|_| anyhow!("Signature nonce is not a pub key"))?,
+    //     ),
+    //     RistrettoSecretKey::from_canonical_bytes(&signature_u)
+    //         .map_err(|_| anyhow!("signature_u is not a valid key"))?,
+    //     RistrettoSecretKey::from_canonical_bytes(&signature_v)
+    //         .map_err(|_| anyhow!("signature_v is not a valid key"))?,
+    // );
+
+    let mut utxos = client
+        .fetch_matching_utxos(FetchMatchingUtxosRequest {
+            hashes: vec![commitment_bytes.clone()],
+            include_spent: true,
+            include_burnt: true,
+        })
+        .await?
+        .into_inner();
+
+    while let Some(utxo) = utxos.next().await {
+        dbg!("found by hash");
+        let inner = utxo.unwrap();
+        let features = inner.output.unwrap().features;
+        if features.unwrap().output_type == 2 {
+            return Ok(true);
+        }
+        // dbg!(inner.output.unwrap().features);
+
+        // return Ok(true);
+        // dbg!(utxo);
+    }
+
     let mut res = client
         .search_utxos(SearchUtxosRequest {
-            commitments: vec![
-                Vec::<u8>::from_hex(commitment).map_err(|e| anyhow!("bad hex commitment"))?
-            ],
+            commitments: vec![commitment_bytes],
         })
         .await?
         .into_inner();
 
     let mut count_utxos = 0;
     while let Some(utxo) = res.next().await {
-        //dbg!(utxo);
+        dbg!(utxo);
         count_utxos += 1;
     }
     Ok(count_utxos > 0)
@@ -100,6 +164,7 @@ async fn check_commitment_exists(commitment: &str) -> Result<bool, anyhow::Error
 async fn main() {
     // teloxide::enable_logging!();
     log::info!("Starting bot...");
+    dotenv().ok();
 
     let bot = Bot::from_env();
 
